9b0aad73e954db017f4e8dde53b975a0a068f835
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-02-28 20:24:47 -0500

    chaos

--- hg-view/src/main.rs
+++ hg-view/src/main.rs
@@ -1,7 +1,15 @@
 use eframe::egui;
 
+use winit::{
+    event::{Event, KeyEvent, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+};
+
+use std::sync::Arc;
+
+
 fn main() -> Result<(), eframe::Error> {
-    //env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
         ..Default::default()
@@ -52,4 +60,63 @@ impl eframe::App for MyApp {
             ));*/
         });
     }
+}
+
+
+
+
+
+
+pub async fn run(window: Window, event_loop: EventLoop<()>) {
+    let window = Arc::new(window);
+
+    let mut state = state::State::new(Arc::clone(&window)).await;
+
+    let _ = event_loop.run(move |event, ewlt| {
+        ewlt.set_control_flow(ControlFlow::Wait);
+
+        match event {
+            Event::AboutToWait => {
+                state.about_to_wait();
+            }
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == state.window().id() => {
+                match event {
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        event:
+                            KeyEvent {
+                                logical_key:
+                                    winit::keyboard::Key::Named(winit::keyboard::NamedKey::Escape),
+                                ..
+                            },
+                        ..
+                    } => ewlt.exit(),
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(*physical_size);
+                    }
+                    WindowEvent::RedrawRequested => match state.render() {
+                        Ok(_) => {}
+                        Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+                            state.resize(state.size)
+                        }
+                        Err(wgpu::SurfaceError::OutOfMemory) => ewlt.exit(),
+                        Err(wgpu::SurfaceError::Timeout) => {
+                            // Ignore timeouts.
+                        }
+                    },
+                    other => {
+                        state.handle_event(other);
+                        window.request_redraw();
+                        return;
+                    }
+                };
+                state.handle_event(event);
+                window.request_redraw();
+            }
+            _ => {}
+        }
+    });
 }
